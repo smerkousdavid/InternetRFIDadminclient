732f7682519cd2c26efa8359a5d2158de611b76b
David Smerkous <smerkousdavid@gmail.com>
2015-07-13 15:31:49 -0600

    Update test.py

--- test.py
+++ test.py
@@ -21,9 +21,21 @@ def year():
 def date():
     print "date press"
 
-def TESTBUTTON():
-    print "Test button pressed"
+def start():
+    while True:
+        print "start"
+        count = 1
+        learnerarray = []
+        for i in range(0,5):
+            learnerarray.append(LTSBackend(count).getData())
+            count = count + 1
 
+        count = 1
+        for i in learnerarray:
+            Gui.setrow(win,i,count)
+            count = count + 1
+        Gui.cleargrid(win)
+        sleep(10)
 count = 1
 learnerarray = []
 for i in range(0,5):
@@ -33,22 +45,7 @@ for i in range(0,5):
 count = 1
 for i in learnerarray:
     Gui.setrow(win,i,count)
-    count = count + 1
+    count +=1
 
-
+start()
 Gui.start()
-
-while True:
-    print "start"
-    count = 1
-    learnerarray = []
-    for i in range(0,5):
-        learnerarray.append(LTSBackend(count).getData())
-        count = count + 1
-
-    count = 1
-    for i in learnerarray:
-        Gui.setrow(win,i,count)
-        count = count + 1
-
-    sleep(10)
